001f9186f9e862cabce763c9dc1b03bf1517481a
余佳兴 <@ORI-YU>
2023-11-09 19:02:31 +0800

    添加项目文件。

---
+++ Assets/Scripts/Controller/GameManger.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    private bool isPause = true;
+    public Shape[] shapes;
+    public Color[] colors;
+    public Shape  currentShape=null;
+    public Controller controller;
+    public GameObject blockHolder;
+    void Start()
+    {
+        controller = GetComponent<Controller>();
+    }
+
+    public void StartGame()
+    {
+        if (currentShape != null)
+            currentShape.StartGame();
+        isPause = false;
+    }
+    public void PauseGame()
+    {
+        if (currentShape != null)
+            currentShape.PauseGame();
+        isPause = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPause) return;
+        if (currentShape == null)
+        {
+            SpawnShape();
+        }
+        
+
+    }
+
+    public void SpawnShape()
+    {
+        currentShape = Instantiate(shapes[Random.Range(0, shapes.Length)]);
+        colors[0]=Color.black; colors[1]=Color.white;
+        currentShape.Init(colors[Random.Range(0,2)],controller,this);
+        //Debug.Log(Random.Range(0, 2));
+        currentShape.transform.SetParent(blockHolder.transform);
+    }
+
+    public void FallDown()
+    {
+        if (controller.model.isUpdateScore)
+        {
+            controller.view.UpdateScoreUI(controller.model.Score, controller.model.BestScore);
+        }
+
+       foreach(Transform child in blockHolder.transform)
+        {
+            if (child.childCount <= 1)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+      
+        if (controller.model.IsGameOver())
+        {
+           
+            PauseGame();
+            controller.view.ShowGameOverUI(controller.model.Score);
+        } 
+          currentShape = null;
+
+    }
+
+    public void ClearCurrentShape()
+    {
+        if (currentShape != null)
+        {
+            Destroy(currentShape.gameObject);
+            currentShape = null;
+        }
+    }
+}
---
+++ NewBehaviourScript1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace PolymorphismApplication
+{
+    abstract class Shape
+    {
+        abstract public int area();
+    }
+    class Rectangle : Shape
+    {
+        private int length;
+        private int width;
+        public Rectangle(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+        public override int area()
+        {
+            Console.WriteLine("Rectangle 类的面积：");
+            return (width * length);
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10, 7);
+            double a = r.area();
+            Console.WriteLine("面积： {0}", a);
+            Console.ReadKey();
+        }
+    }
+}
